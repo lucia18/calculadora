9d9816db69e56e2a523077e3f2d71f68968cfda0
sergiom1996 <sergimonteagudo@hotmail.com>
2017-11-20 19:22:26 +0100

    multiplicacion

--- src/ed1/ejercicio1.java
+++ src/ed1/ejercicio1.java
@@ -1,10 +1,27 @@
 package ed1;
 
+import java.util.Scanner;
+
 public class ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int num1;
+		int num2;
+		int resultado=0;
+		Scanner teclado = new Scanner(System.in);
+				
+		System.out.println("introduce un numero:");
+		num1=teclado.nextInt();
+		
+		
+		System.out.println("introduce otro numero:");
+		num2=teclado.nextInt();
+		
+		resultado=num1*num2;
+		
+		System.out.println("El resultado es " +resultado);
 	}
 
 }
