e4edfea0fd08024d5c384bdc73b236e6c99c6629
lucia18 <luciaaserrano18gmail.com>
2017-11-20 19:20:44 +0100

    suma

--- src/ed1/ejercicio1.java
+++ src/ed1/ejercicio1.java
@@ -1,10 +1,18 @@
 package ed1;
 
+import java.util.Scanner;
+
 public class ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Introduce un número:");
+		String n1=teclado.nextLine();
+		System.out.println("Introduce otro número:");
+		String n2=teclado.nextLine();
+		String resultado;
+		resultado=n1+n2;
 	}
 
 }
