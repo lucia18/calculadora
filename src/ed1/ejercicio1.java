65c71f294328517fb00e07a6635eb0cab582249b
crist1an <cristian.miranda.freire@gmail.com>
2017-11-20 20:26:05 +0100

    final calcula suma resta y multi

--- src/ed1/ejercicio1.java
+++ src/ed1/ejercicio1.java
@@ -10,31 +10,50 @@ public class ejercicio1 {
 		int num1=0;
 		int num2=0;
 		int ope=0;//numero que nos dira que operacion hacemos
-		System.out.println("Dame el primer número:");
-		num1=teclado.nextInt();
-		System.out.println("Dame el segundo número:");
-		num2=teclado.nextInt();
-		System.out.println("Que deseas hacer");
-		System.out.println("1-Suma");
-		System.out.println("2-Resta");
-		System.out.println("3-Multiplicacion");
-		System.out.println("4-Division");
-		ope=teclado.nextInt();
-		switch(ope){
-		case 1: ope=num1+num2;
-		System.out.println("El resultado de "+num1+ " + "+num2+" es ="+ope);
-		break;
-		case 2: ope=num1-num2;
-		System.out.println("El resultado de "+num1+ " - "+num2+" es = "+ope);
-		break;
-		case 3: ope=num1*num2;
-		System.out.println("El resultado de "+num1+ " x "+num2+" es = "+ope);
-		break;
-		case 4: ope=num1/num2;
-		System.out.println("El resultado de "+num1+ " : "+num2+" es = "+ope);
-		}
+	
+		int repe=1;//elegir opcion salir o entrar
+		System.out.println("Pulse 1 si quiere hacer una operacicon");
+		System.out.println("Pulse 2 para salir");
+		repe=teclado.nextInt();
+		while(repe==1){
+			
+			System.out.println("Dame el primer número:");//pide primer numero
+			num1=teclado.nextInt();
+			System.out.println("Dame el segundo número:");// segundo numero
+			num2=teclado.nextInt();
+			System.out.println("Que deseas hacer");//pide una operacion
+			System.out.println("1-Suma");
+			System.out.println("2-Resta");
+			System.out.println("3-Multiplicacion");
+			System.out.println("4-Division");
+			ope=teclado.nextInt();
+			switch(ope){
+			case 1: ope=num1+num2;
+			System.out.println("El resultado de "+num1+ " + "+num2+" es ="+ope);//suma
+			break;
+			case 2: ope=num1-num2;
+			System.out.println("El resultado de "+num1+ " - "+num2+" es = "+ope);//resta
+			break;
+			case 3: ope=num1*num2;
+			System.out.println("El resultado de "+num1+ " x "+num2+" es = "+ope);//multiplicacion
+			break;
 
-	}
+			case 4: 
+				if(num2<=0){
+					System.out.println("Vuelva a introducir los numeros");//division
+					
+				}else{
+					ope=num1/num2;
+					System.out.println("El resultado de "+num1+ " : "+num2+" es = "+ope);
+				}
+			}
+			System.out.println("Pulse 1 si quiere hacer una operacicon");
+			System.out.println("Pulse 2 para salir");
+			repe=teclado.nextInt();
+			
+		}
+		}
+		
 
 
 }
