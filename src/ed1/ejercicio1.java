f55cbcfa3aa3cd7e008e554800010b9a119d6eaf
crist1an <cristian.miranda.freire@gmail.com>
2017-11-20 19:40:48 +0100

    menu resta

--- src/ed1/ejercicio1.java
+++ src/ed1/ejercicio1.java
@@ -1,10 +1,35 @@
 package ed1;
 
+import java.util.Scanner;
+
 public class ejercicio1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		int num1=0;
+		int num2=0;
+		int ope=0;//numero que nos dira que operacion hacemos
+		System.out.println("Dame el primer número:");
+		num1=teclado.nextInt();
+		System.out.println("Dame el segundo número:");
+		num2=teclado.nextInt();
+		System.out.println("Que deseas hacer");
+		System.out.println("1-Suma");
+		System.out.println("2-Resta");
+		System.out.println("3-Multiplicacion");
+		ope=teclado.nextInt();
+		switch(ope){
+		case 1: 
+		break;
+		case 2: ope=num1-num2;
+		System.out.println("El resultado de "+num1+ " - "+num2+" es = "+ope);
+		break;
+		case 3: 
+		
 
 	}
 
 }
+	}
+	
